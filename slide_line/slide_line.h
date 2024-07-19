aca65f161bd1aebd4a1c0817e869e844df324b8a
shane <6874@holbertonstudents.com>
2024-07-19 10:09:43 +0200

    task 0

--- slide_line/slide_line.h
+++ slide_line/slide_line.h
@@ -3,7 +3,7 @@
 
 #include <stddef.h>  // Include for size_t definition
 
-#define SLIDE_LEFT  0
+#define SLIDE_LEFT 0
 #define SLIDE_RIGHT 1
 
 int slide_line(int *line, size_t size, int direction);
