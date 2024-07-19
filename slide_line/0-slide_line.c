a4c176321e115904abdd1c172d008253f0902966
shane <6874@holbertonstudents.com>
2024-07-19 10:16:07 +0200

    task 0 corrected

--- slide_line/0-slide_line.c
+++ slide_line/0-slide_line.c
@@ -10,77 +10,94 @@
 int slide_line(int *line, size_t size, int direction)
 {
 	if (direction != SLIDE_LEFT && direction != SLIDE_RIGHT)
-		return 0;
+		return (0);
 
 	if (direction == SLIDE_LEFT)
-	{
-		int i, current = 0, merged = 0;
-
-		for (i = 0; i < (int)size; i++)
-		{
-			if (line[i] != 0)
-			{
-				if (current != i)
-				{
-					if (line[current] == line[i] && merged == 0)
-					{
-						line[current++] *= 2;
-						line[i] = 0;
-						merged = 1;
-					}
-					else if (line[current] == 0)
-					{
-						line[current] = line[i];
-						line[i] = 0;
-					}
-					else
-					{
-						if (line[current + 1] == 0)
-							current++;
-						line[++current] = line[i];
-						if (i != current)
-							line[i] = 0;
-					}
-				}
-				else
-					merged = 0;
-			}
-		}
-	}
+		slide_left(line, size);
 	else if (direction == SLIDE_RIGHT)
-	{
-		int i, current = size - 1, merged = 0;
+		slide_right(line, size);
 
-		for (i = size - 1; i >= 0; i--)
+	return (1);
+}
+
+/**
+ * slide_left - Slides and merges an array of integers to the left.
+ * @line: Pointer to an array of integers.
+ * @size: Number of elements in @line.
+ */
+void slide_left(int *line, size_t size)
+{
+	int i, current = 0, merged = 0;
+
+	for (i = 0; i < (int)size; i++)
+	{
+		if (line[i] != 0)
 		{
-			if (line[i] != 0)
+			if (current != i)
 			{
-				if (current != i)
+				if (line[current] == line[i] && merged == 0)
 				{
-					if (line[current] == line[i] && merged == 0)
-					{
-						line[current--] *= 2;
-						line[i] = 0;
-						merged = 1;
-					}
-					else if (line[current] == 0)
-					{
-						line[current] = line[i];
-						line[i] = 0;
-					}
-					else
-					{
-						if (line[current - 1] == 0)
-							current--;
-						line[--current] = line[i];
-						if (i != current)
-							line[i] = 0;
-					}
+					line[current++] *= 2;
+					line[i] = 0;
+					merged = 1;
+				}
+				else if (line[current] == 0)
+				{
+					line[current] = line[i];
+					line[i] = 0;
 				}
 				else
-					merged = 0;
+				{
+					if (line[current + 1] == 0)
+						current++;
+					line[++current] = line[i];
+					if (i != current)
+						line[i] = 0;
+				}
 			}
+			else
+				merged = 0;
 		}
 	}
-	return 1;
+}
+
+/**
+ * slide_right - Slides and merges an array of integers to the right.
+ * @line: Pointer to an array of integers.
+ * @size: Number of elements in @line.
+ */
+void slide_right(int *line, size_t size)
+{
+	int i, current = size - 1, merged = 0;
+
+	for (i = size - 1; i >= 0; i--)
+	{
+		if (line[i] != 0)
+		{
+			if (current != i)
+			{
+				if (line[current] == line[i] && merged == 0)
+				{
+					line[current--] *= 2;
+					line[i] = 0;
+					merged = 1;
+				}
+				else if (line[current] == 0)
+				{
+					line[current] = line[i];
+					line[i] = 0;
+				}
+				else
+				{
+					if (line[current - 1] == 0)
+						current--;
+					line[--current] = line[i];
+					if (i != current)
+						line[i] = 0;
+				}
+			}
+			else
+				merged = 0;
+		}
+	}
 }
--- slide_line/slide_line.h
+++ slide_line/slide_line.h
@@ -1,11 +1,13 @@
 #ifndef SLIDE_LINE_H
 #define SLIDE_LINE_H
 
-#include <stddef.h>  // Include for size_t definition
+#include <stddef.h>
 
 #define SLIDE_LEFT 0
 #define SLIDE_RIGHT 1
 
 int slide_line(int *line, size_t size, int direction);
+void slide_left(int *line, size_t size);
+void slide_right(int *line, size_t size);
 
 #endif /* SLIDE_LINE_H */
