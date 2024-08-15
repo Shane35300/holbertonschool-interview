99f21306c5f540ae33fa3fa4c61d639bd8458abd
shane <6874@holbertonstudents.com>
2024-08-15 14:28:25 +0200

    task 0

--- linear_skip/0-linear_skip.c
+++ linear_skip/0-linear_skip.c
@@ -7,44 +7,39 @@
  * @list: Pointer to the head of the skip list
  * @value: The value to search for
  *
- * Return: Pointer to the first node where value is located, or NULL if not found
+ * Return: Pointer to the first node where value is located, NULL if not found
  */
 skiplist_t *linear_skip(skiplist_t *list, int value)
 {
-	skiplist_t *current = list;
-	skiplist_t *prev = NULL;
+skiplist_t *node = list;
+skiplist_t *prev = NULL;
 
-	if (list == NULL)
-		return (NULL);
+if (list == NULL)
+	return (NULL);
 
-	// Use the express lane to skip ahead
-	while (current->express && current->express->n < value)
-	{
-		printf("Value checked at index [%lu] = [%d]\n",
-			   current->express->index, current->express->n);
-		prev = current;
-		current = current->express;
-	}
+while (node->express != NULL && node->express->n < value)
+{
+	prev = node;
+	node = node->express;
+	printf("Value checked at index [%lu] = [%d]\n", node->index, node->n);
+}
 
-	if (current->express)
-		printf("Value found between indexes [%lu] and [%lu]\n",
-			   prev ? prev->index : 0, current->express->index);
-	else
-		printf("Value found between indexes [%lu] and [%lu]\n",
-			   prev ? prev->index : 0, current->index);
+if (node->express != NULL)
+	printf("Value found between indexes [%lu] and [%lu]\n",
+			prev ? prev->index : 0, node->express->index);
+else
+	printf("Value found between indexes [%lu] and [%lu]\n",
+			prev ? prev->index : 0, node->index);
 
-	// Linear search within the block
-	while (current && current->n < value)
-	{
-		printf("Value checked at index [%lu] = [%d]\n",
-			   current->index, current->n);
-		current = current->next;
-	}
-	if (current)
-	{
-		printf("Value checked at index [%lu] = [%d]\n",
-			   current->index, current->n);
-	}
+node = prev ? prev : list;
+while (node != NULL &&
+	node->index <= (node->express ? node->express->index : SIZE_MAX))
+{
+	printf("Value checked at index [%lu] = [%d]\n", node->index, node->n);
+	if (node->n == value)
+		return (node);
+	node = node->next;
+}
 
-	return (current && current->n == value) ? current : NULL;
+return (NULL);
 }
