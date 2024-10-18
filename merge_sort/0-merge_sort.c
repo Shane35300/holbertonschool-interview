b2d3a163d4418adb91cd2c7a4e6238a2868742c2
shane <6874@holbertonstudents.com>
2024-10-18 18:00:00 +0200

    correcting according to the instructions

--- merge_sort/0-merge_sort.c
+++ merge_sort/0-merge_sort.c
@@ -6,85 +6,59 @@
  * merge - Fusionne deux sous-tableaux triés en un seul tableau trié
  *
  * @array: Le tableau d'origine
- * @left: Le sous-tableau de gauche
- * @right: Le sous-tableau de droite
- * @size_left: La taille du sous-tableau de gauche
- * @size_right: La taille du sous-tableau de droite
+ * @temp: Tableau temporaire utilisé pour la fusion
+ * @left: Index du début du sous-tableau de gauche
+ * @mid: Milieu du tableau, séparant les deux sous-tableaux
+ * @right: Index de fin du sous-tableau de droite
  */
-void merge(int *array, int *left, int *right, size_t size_left,
-size_t size_right)
+void merge(int *array, int *temp, size_t left, size_t mid, size_t right)
 {
-	size_t i = 0, j = 0, k = 0;
+	size_t i = left, j = mid, k = left;
 
 	printf("Merging...\n[left]: ");
-	print_array(left, size_left);
+	print_array(array + left, mid - left);
 	printf("[right]: ");
-	print_array(right, size_right);
+	print_array(array + mid, right - mid);
 
-	while (i < size_left && j < size_right)
+	while (i < mid && j < right)
 	{
-		if (left[i] <= right[j])
-		{
-			array[k++] = left[i++];
-		}
+		if (array[i] <= array[j])
+			temp[k++] = array[i++];
 		else
-		{
-			array[k++] = right[j++];
-		}
+			temp[k++] = array[j++];
 	}
-	while (i < size_left)
-	{
-		array[k++] = left[i++];
-	}
-	while (j < size_right)
-	{
-		array[k++] = right[j++];
-	}
+
+	while (i < mid)
+		temp[k++] = array[i++];
+	while (j < right)
+		temp[k++] = array[j++];
+
+	for (i = left; i < right; i++)
+		array[i] = temp[i];
 
 	printf("[Done]: ");
-	print_array(array, size_left + size_right);
+	print_array(array + left, right - left);
 }
 
 /**
  * merge_sort_recursive - Fonction récursive pour le tri par fusion
  *
  * @array: Le tableau à trier
- * @size: La taille du tableau
+ * @temp: Tableau temporaire utilisé pour la fusion
+ * @left: Index du début du sous-tableau
+ * @right: Index de fin du sous-tableau
  */
-void merge_sort_recursive(int *array, size_t size)
+void merge_sort_recursive(int *array, int *temp, size_t left, size_t right)
 {
-	if (size < 2)
-	{
+	if (right - left < 2)
 		return;
-	}
 
-	size_t mid = size / 2;
-	int *left = malloc(mid * sizeof(int));
-	int *right = malloc((size - mid) * sizeof(int));
+	size_t mid = (left + right) / 2;
 
-	if (!left || !right)
-	{
-		free(left);
-		free(right);
-		return;
-	}
+	merge_sort_recursive(array, temp, left, mid);
+	merge_sort_recursive(array, temp, mid, right);
 
-	for (size_t i = 0; i < mid; i++)
-	{
-		left[i] = array[i];
-	}
-	for (size_t i = mid; i < size; i++)
-	{
-		right[i - mid] = array[i];
-	}
-
-	merge_sort_recursive(left, mid);
-	merge_sort_recursive(right, size - mid);
-
-	merge(array, left, right, mid, size - mid);
-
-	free(left);
-	free(right);
+	merge(array, temp, left, mid, right);
 }
 
 /**
@@ -96,9 +70,14 @@ void merge_sort_recursive(int *array, size_t size)
 void merge_sort(int *array, size_t size)
 {
 	if (!array || size < 2)
-	{
 		return;
-	}
 
-	merge_sort_recursive(array, size);
+	int *temp = malloc(size * sizeof(int));
+
+	if (!temp)
+		return;
+
+	merge_sort_recursive(array, temp, 0, size);
+
+	free(temp);
 }
