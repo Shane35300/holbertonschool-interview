44fef98db727852ff538bb554eeb2389733f827b
shane <6874@holbertonstudents.com>
2024-11-11 10:14:52 +0100

    task 0

---
+++ island_perimeter/0-island_perimeter.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+This module's goal is to calculate the right permieter"""
+
+
+def island_perimeter(grid):
+    """
+    This function calculate a perimeter
+    """
+    total = 0
+    i = 0
+    for i in range(len(grid)):
+        j = 0
+        for j in range(len(grid[i])):
+            if grid[i][j] == 1:
+                per = 0
+                if grid[i - 1][j] != 1:
+                    per += 1
+                if grid[i + 1][j] != 1:
+                    per += 1
+                if grid[i][j - 1] != 1:
+                    per += 1
+                if grid[i][j + 1] != 1:
+                    per += 1
+                total += per
+    return (total)
