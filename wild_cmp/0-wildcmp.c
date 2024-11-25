aadaf05789621b90adbb2a8509fb23d4f6d91f1c
shane <6874@holbertonstudents.com>
2024-11-25 11:52:56 +0100

    with betty correction

--- wild_cmp/0-wildcmp.c
+++ wild_cmp/0-wildcmp.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 
 /**
- * wildcmp - Compares two strings and checks if they can be considered identical
+ * wildcmp - Compares two strings and checks if they can be considered identica
  * @s1: The first string
  * @s2: The second string, which can include the wildcard '*'
  *
@@ -9,20 +9,20 @@
  */
 int wildcmp(char *s1, char *s2)
 {
-    if (*s2 == '\0')
-        return (*s1 == '\0');
+if (*s2 == '\0')
+	return (*s1 == '\0');
 
-    if (*s2 == '*')
-    {
-        if (*(s2 + 1) == '\0')
-            return (1);
-        if (*s1 == '\0')
-            return (wildcmp(s1, s2 + 1));
-        return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
-    }
+if (*s2 == '*')
+{
+	if (*(s2 + 1) == '\0')
+		return (1);
+	if (*s1 == '\0')
+		return (wildcmp(s1, s2 + 1));
+	return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
+}
 
-    if (*s1 == *s2)
-        return (wildcmp(s1 + 1, s2 + 1));
+if (*s1 == *s2)
+	return (wildcmp(s1 + 1, s2 + 1));
 
-    return (0);
+return (0);
 }
